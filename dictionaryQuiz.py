4ad7f5d179b61b9529f260c52eb77cd29858a883
davaraydinata2003 <128357486+davaraydinata2003@users.noreply.github.com>
2023-06-13 13:54:56 +0700

    Add files via upload

---
+++ dictionaryQuiz.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[12]:
+
+
+
+a = {(1,2):1,(2,3):2}
+print(a[1,2])
+
+a = {'a':1,'b':2,'c':3}
+print (a['a','b'])
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
