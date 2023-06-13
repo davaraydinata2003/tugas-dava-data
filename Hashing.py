517f4a0b894bd40c6f2099926e4302b680ff2313
davaraydinata2003 <128357486+davaraydinata2003@users.noreply.github.com>
2023-06-13 14:42:40 +0700

    Add files via upload

---
+++ Hashing.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[35]:
+
+
+def mod(number, cellNumber):
+    return number % cellNumber
+
+
+# print(mod(400, 24))
+
+
+def modASCII(string, cellNumber):
+    total = 0
+    for i in string:
+        total += ord(i)
+    return total % cellNumber
+
+print(modASCII("ABC", 24))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
