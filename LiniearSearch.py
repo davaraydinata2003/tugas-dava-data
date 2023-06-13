b85dffecad4effd160065b65f92e52b1809ae3ab
davaraydinata2003 <128357486+davaraydinata2003@users.noreply.github.com>
2023-06-13 14:57:30 +0700

    Add files via upload

---
+++ LiniearSearch.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[49]:
+
+
+def linearSearch(array, value):
+    for i in range(len(array)):
+        if array[i] == value:
+            return i
+    return -1
+
+
+
+print(linearSearch([20,40,30,50,90], 90))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
