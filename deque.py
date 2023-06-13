47a52eb61e53ff3e4e8c80fb76fe2529e43b2d42
davaraydinata2003 <128357486+davaraydinata2003@users.noreply.github.com>
2023-06-13 14:23:05 +0700

    Add files via upload

---
+++ deque.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[19]:
+
+
+from collections import deque
+
+customQueue = deque(maxlen=3)
+print(customQueue)
+
+customQueue.append(1)
+customQueue.append(2)
+customQueue.append(3)
+customQueue.append(4)
+print(customQueue)
+print(customQueue.clear())
+print(customQueue)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
---
+++ multiprocessingqueue.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[23]:
+
+
+from multiprocessing import Queue
+
+customQueue = Queue(maxsize= 3)
+customQueue.put(1)
+print(customQueue.get())
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
